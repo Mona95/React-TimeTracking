28fc9b6e6a29dc35a906fad7cc765790b5ad6e17
39981838 <shamona9594@gmail.com>
2020-01-26 11:36:50 +0330

    semanticUI added + related changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function App() {
-  return <div>Time Tracking App</div>;
+  return (
+    <div className="main ui">
+      <h1
+        className="ui dividing centered header
+  "
+      >
+        Time Tracking App
+      </h1>
+    </div>
+  );
 }
 
 export default App;
